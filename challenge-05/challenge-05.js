192ad7c64c29ca27e5b6e76934107024f35c5583
Matheus Ferraz <45341918+Shamamat@users.noreply.github.com>
2019-12-30 02:15:39 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var qualquer = [5, 'Matheuszinho Gameplays', true, undefined, 19 ]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function retornar(){
+  return qualquer
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+qualquer[1]
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,18 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function param([1, 'alguma coisa', true, 'olá!', 234])
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var qualquer2 = [5, 'Matheuszinho Gameplays', true, undefined, null ]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+qualquer2[4]
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,7 +49,8 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(nomeLivro){
+  var nome = 
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
