4aef89fa969467969c94b5c2e939e409a67ad0ab
Matheus Ferraz <45341918+Shamamat@users.noreply.github.com>
2019-12-31 21:47:21 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -14,7 +14,7 @@ function retornar(){
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-qualquer[1]
+qualquer[1] // OBS: Ele colocou como console.log por agora ser no js/node, mas vou mudar tbm quando chegar html
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -22,7 +22,9 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-function param([1, 'alguma coisa', true, 'olá!', 234])
+function param(arr, index){
+  return arr[index] // * Correção/Obs: Essa parte eu só copiei pq realmente n tinha entendido exatamente, mas é até que simples
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
@@ -33,7 +35,8 @@ var qualquer2 = [5, 'Matheuszinho Gameplays', true, undefined, null ]
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-qualquer2[4]
+qualquer2[4] // Repetindo, podia ser console.log igual o professor fez: console.log( myFunction2( myArray2, 0) );
+             // OBS: ele colocou todos os valores um por um por mais que esteja "último" na pergunta 
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -50,19 +53,42 @@ propriedades:
 os livros.
 */
 function book(nomeLivro){
-  var nome = 
-
+  var nomesLivros = { livro1: 'Harry Potter', livro2: 'O senhor dos anéis', livro3: 'IT a coisa' }
+  var livroHarry = { quantidadePaginas: 200, autor: 'Philipino', editora: 'Panini' }
+  var livroSenhorAneis = { quantidadePaginas: 300, autor: 'Marco Lutariti', editora: 'Hihappy' }
+  var livroIT = { quantidadePaginas: 400, autor: 'Coringa', editora: 'Saraiva' }
+  var tudo = `Coloque algum livro dos 3 livros: ${nomesLivros}`
+/* *Correção importante: Ele passou os nomes dos livros diretamente com o nome e com suas informações, usando por exemplo: 
+   'Introdução ao HTML5': { quantidadePaginas: 220, autor: 'John Resig & Bear Bibeault', Editora: 'Novatec' }, (e colocava os outros 2)
+    Mas, ele não pode ser usado como variável por ele ser um 'nome' e não uma 'variável'... mas o meu tbm n ta errado, só mais longo */
+  
+  if(nomeLivro === nomesLivros.livro1){
+    return livroHarry
+     }
+  else if(nomeLivro === nomesLivros.livro2){
+     return livroSenhorAneis
+     }
+  else if(nomeLivro === nomesLivros.livro3){ 
+    return livroIT
+     } // *Correção: Na parte de mostrar o livro certo ele só colocou: return !bookName ? allBooks : allbooks[bookName]; :v
+  else{
+    return tudo
+     }
+  }
+  
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book('') //Também podia ser qualquer outra palavra que não seja um dos livros, e ele colocou o console.log
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+/* Essa parte aq n entendi exatamente oq ele fez, eu pensei que poderia ser criar uma variável com essa frase dentro de book
+(que ficaria melhor na minha opinião) mas ele fez:  */
+book()
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
